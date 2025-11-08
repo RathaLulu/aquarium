ab6f5098fc53633dbf70a7fa4edc620e16068bd1
Lulu <lucasmeerschaut@yahoo.com>
2025-11-08 14:36:28 +0100

    qa : add test for basic aquarium

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <vector>
 
 #include "poisson.h"
+#include "aquarium.h"
 
 int main() 
 {
-    Poisson fish("Fish", Sexe::MALE);
-    fish.sayHello();
+    Poisson fish1("Hector", Sexe::MALE);
+    Poisson fish2("Nemo", Sexe::MALE);
+    Poisson fish3("Carle", Sexe::FEMELLE);
+    Poisson fish4("COLLE", Sexe::FEMELLE);
+    int Nalges = 100; 
+    std::vector<Poisson> Poissons {fish1, fish2, fish3}; 
+    Aquarium Aqua(Poissons, Nalges);
+    Aqua.printInfo();
+    Aqua.addAlges();
+    Aqua.addAlges(50);
+    Aqua.addPoisson(fish4);
+    Aqua.printInfo();
+    return 1; 
 }
