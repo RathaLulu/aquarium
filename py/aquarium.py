40eb2e6028eb7ed2f15d179cd85f3caa385b11f6
Lulu <lucasmeerschaut@yahoo.com>
2025-11-27 19:50:07 +0100

    feat: color choice for fish

--- py/aquarium.py
+++ py/aquarium.py
@@ -12,7 +12,7 @@ class aquarium :
     def __init__(self, json_epoch, surface):
         self.Nepoch = json_epoch['tour']
         self.surface = surface
-        self.poissons = [poisson(p, RED, self.surface) for p in json_epoch['poissons']]
+        self.poissons = [poisson(p,self.surface) for p in json_epoch['poissons']]
 
     def draw(self) :
         for poisson in self.poissons: 
@@ -20,7 +20,7 @@ class aquarium :
 
 class poisson: 
 
-    def __init__(self, json_poisson, color, surface): 
+    def __init__(self, json_poisson, surface): 
         self.json = json_poisson
         self.name = json_poisson['name']
         self.type = json_poisson['type']
@@ -28,7 +28,6 @@ class poisson:
         self.V = np.array([json_poisson['pos']['vx'], json_poisson['pos']['vy']])
         self.flip = self.V[0]<0
         self.pv = json_poisson['life']
-        self.color = color
         self.surface = surface 
 
     def draw(self):
@@ -36,6 +35,14 @@ class poisson:
         Dessine un poisson simple.
         flip=True : le poisson regarde vers la gauche
         """
+
+        if self.type == 'CARNIVORE':
+            color = RED
+        elif self.type == 'HERBIVORE': 
+            color = GREEN
+        else : 
+            color = ORANGE
+
 
         WIDTH, HEIGHT = self.surface.get_size()
         x = (self.X[0]) / 20 * WIDTH *2
@@ -48,17 +55,18 @@ class poisson:
 
         if self.flip:
             # Corps
-            pygame.draw.ellipse(self.surface, self.color, (x - body_width, y, body_width, body_height))
+            pygame.draw.ellipse(self.surface, color, (x - body_width, y, body_width, body_height))
             # Queue
             tail = [(x, y + body_height // 2),
                     (x + tail_size, y),
                     (x + tail_size, y + body_height)]
-            pygame.draw.polygon(self.surface, self.color, tail)
+            pygame.draw.polygon(self.surface, color, tail)
         else:
             # Corps
-            pygame.draw.ellipse(self.surface, self.color, (x, y, body_width, body_height))
+            pygame.draw.ellipse(self.surface, color, (x, y, body_width, body_height))
             # Queue
             tail = [(x, y + body_height // 2),
                     (x - tail_size, y),
                     (x - tail_size, y + body_height)]
-            pygame.draw.polygon(self.surface, self.color, tail)
+            pygame.draw.polygon(self.surface, color, tail)
+
